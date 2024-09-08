ffd389788c8ad574d8de054bdac86a6a873796e9
World X <mario13x12@gmail.com>
2024-09-08 10:43:39 -0700

    Installed dependencies for fuwari

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://github.com/World-X/grupo932.git",
   base: "/",
   trailingSlash: "always",
   integrations: [
