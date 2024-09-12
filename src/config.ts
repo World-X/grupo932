a3f13882698a41e39b8f47be0f69cc4667b1ca51
World-X <Mario13X12@gmail.com>
2024-09-12 12:22:42 -0700

    Updated page icon

--- src/config.ts
+++ src/config.ts
@@ -26,7 +26,7 @@ export const siteConfig: SiteConfig = {
   },
   favicon: [    // Leave this array empty to use the default favicon
     {
-      src: '/src/assets/images/avatar-circle.png',    // Path of the favicon, relative to the /public directory
+      src: '/src/assets/images/avatar-icon.ico',    // Path of the favicon, relative to the /public directory
       //theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
       //sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
     }
